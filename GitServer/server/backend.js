d51c20622ccfedd0d802da5e028ce00ab79c18a5
Augusto Vicente <augrovi@hotmail.com>
2022-02-16 22:55:42 -0300

    progress on register commit

--- GitServer/server/backend.js
+++ GitServer/server/backend.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 const fs = require('fs');
 
-app.get('commits-unregistered',(req,res) => {
+app.get('/commits-unregistered',(req,res) => {
 
     const commits_folder = '../commits-register/';
     const commits_files = [];
@@ -14,13 +14,13 @@ app.get('commits-unregistered',(req,res) => {
             {
                 commits_files.push(JSON.parse(fs.readFileSync(commits_folder+file)));
             }
-            res.json(JSON.stringify(commits_files.filter(commit => commit.commit_status === 'unregistered')));
         });
+        res.json(JSON.stringify(commits_files.filter(commit => commit.commit_status === 'unregistered')));
     });
     
 })
 
-app.get('commits-unregistered',(req,res) => {
+app.get('/commits-unregistered',(req,res) => {
 
     const commits_folder = '../commits-register/';
     const commits_files = [];
@@ -31,10 +31,15 @@ app.get('commits-unregistered',(req,res) => {
             {
                 commits_files.push(JSON.parse(fs.readFileSync(commits_folder+file)));
             }
-            res.json(JSON.stringify(commits_files.filter(commit => commit.commit_status === 'registered')));
         });
+        res.json(JSON.stringify(commits_files.filter(commit => commit.commit_status === 'registered')));
     });
     
+})
+
+app.get('/register-commit', (req,res) =>
+{
+    res.sendFile(__dirname+'/register-commit.html');
 })
 
 app.listen(PORT ,()=>console.log(`Connected to ${PORT}`))
